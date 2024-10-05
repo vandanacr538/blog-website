3236fa128e4db4bb6959982acba0b3edbc9154b6
vandanacr538 <vandanacr538@gmail.com>
2024-10-05 20:37:22 +0530

    Handle form changes and log form data

--- src/components/BlogForm/BlogForm.js
+++ src/components/BlogForm/BlogForm.js
@@ -1,22 +1,65 @@
+import { useState } from "react";
 import Modal from "../UI/Modal";
 
 const BlogForm = (props) => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const imageUrlChangeHandler = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const addBlogHandler=(event)=>{
+    event.preventDefault();
+    const blogData={
+        imageUrl: imageUrl,
+        title: title,
+        description: description,
+    }
+    console.log(blogData);
+    props.onClose();
+  }
+
   return (
     <Modal>
       <form>
         <div>
           <label htmlFor="image-url">Image Url</label>
-          <input type="url" id="image-url"></input>
+          <input
+            type="url"
+            id="image-url"
+            value={imageUrl}
+            onChange={imageUrlChangeHandler}
+          ></input>
         </div>
         <div>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title"></input>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={titleChangeHandler}
+          ></input>
         </div>
         <div>
           <label htmlFor="description">Blog Description</label>
-          <input type="text" id="description"></input>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={descriptionChangeHandler}
+          ></input>
         </div>
-        <button type="submit">POST BLOG</button>
+        <button type="submit" onClick={addBlogHandler}>POST BLOG</button>
         <button onClick={props.onClose}>Close</button>
       </form>
     </Modal>
