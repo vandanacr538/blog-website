e4450efed11e07d3c034dcf295ed50ba6c8079c5
vandanacr538 <vandanacr538@gmail.com>
2024-10-08 23:24:56 +0530

    Added empty field validation to form

--- src/components/BlogForm/BlogForm.js
+++ src/components/BlogForm/BlogForm.js
@@ -9,6 +9,7 @@ const BlogForm = () => {
   const [imageUrl, setImageUrl] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState({});
 
   useEffect(()=>{
     if(editForm){
@@ -19,40 +20,75 @@ const BlogForm = () => {
   }, [editForm]);
 
   const imageUrlChangeHandler = (event) => {
+    if(event.target.value.trim().length>0 && errors.imageUrlError){
+      setErrors((prevError)=>{
+        return {...prevError, imageUrlError: ""};
+      });
+    }
     setImageUrl(event.target.value);
   };
 
   const titleChangeHandler = (event) => {
+    if(event.target.value.trim().length>0 && errors.titleError){
+      setErrors((prevError)=>{
+        return {...prevError, titleError: ""};
+      });
+    }
     setTitle(event.target.value);
   };
 
   const descriptionChangeHandler = (event) => {
+    if(event.target.value.trim().length>0 && errors.descriptionError){
+      setErrors((prevError)=>{
+        return {...prevError, descriptionError: ""};
+      });
+    }
     setDescription(event.target.value);
   };
 
+  const formValidation=()=>{
+    let errorList={};
+    let valid=true;
+    if(imageUrl.trim().length===0){
+      errorList.imageUrlError="Image Url field should not be empty!";
+      valid=false;
+    }
+    if(title.trim().length===0){
+      errorList.titleError="Title field should not be empty";
+      valid=false;
+    }
+    if(description.trim().length===0){
+      errorList.descriptionError="Description field should not be empty";
+      valid=false;
+    }
+    setErrors(errorList);
+    return valid;
+  }
+
   const blogFormSubmitHandler=(event)=>{
     event.preventDefault();
-    console.log("open")
-    const newID = Math.random().toString();
-    const blogData={
-        id: (editForm ? editForm.id : newID),
-        imageUrl: imageUrl,
-        title: title,
-        description: description,
+    if(formValidation()){
+      const newID = Math.random().toString();
+      const blogData={
+          id: (editForm ? editForm.id : newID),
+          imageUrl: imageUrl,
+          title: title,
+          description: description,
+      }
+      if(editForm){
+          editBlog(blogData);
+      }
+      else{
+          addBlog(blogData);
+      }
+      closeFormHandler();
     }
-    if(editForm){
-        editBlog(blogData);
-    }
-    else{
-        addBlog(blogData);
-    }
-    closeFormHandler();
   }
 
   return (
     <Modal>
       <form className="blog-form">
-        <div className="blog-control img-control"> 
+        <div className={`blog-control img-control ${errors.imageUrlError ? "invalid" : ""}`}> 
           <label htmlFor="image-url">Image Url</label>
           <input
             type="url"
@@ -60,8 +96,9 @@ const BlogForm = () => {
             value={imageUrl}
             onChange={imageUrlChangeHandler}
           ></input>
+          {errors.imageUrlError && <p>{errors.imageUrlError}</p>}
         </div>
-        <div className="blog-control">
+        <div className={`blog-control ${errors.titleError ? "invalid" : ""}`}>
           <label htmlFor="title">Title</label>
           <input
             type="text"
@@ -69,8 +106,9 @@ const BlogForm = () => {
             value={title}
             onChange={titleChangeHandler}
           ></input>
+          {errors.titleError && <p>{errors.titleError}</p>}
         </div>
-        <div className="blog-control">
+        <div className={`blog-control ${errors.descriptionError ? "invalid" : ""}`}>
           <label htmlFor="description">Blog Description</label>
           <input
             type="text"
@@ -78,6 +116,7 @@ const BlogForm = () => {
             value={description}
             onChange={descriptionChangeHandler}
           ></input>
+          {errors.descriptionError && <p>{errors.descriptionError}</p>}
         </div>
         <div className="blog-form-actions">
           <button type="submit" onClick={blogFormSubmitHandler}>
